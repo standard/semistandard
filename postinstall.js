2d8fb74a02c329fd35279b909fd6d922c758c9f8
Dan Flettre <fletd01@yahoo.com>
2015-01-29 23:22:57 -0600

    update to standard ~2.0.0

--- postinstall.js
+++ postinstall.js
@@ -4,11 +4,17 @@ var replace = require('replace');
 replace({
   regex: '"asi": true',
   replacement: '"asi": false',
-  paths: [path.join(__dirname, 'node_modules', 'standard', 'lib', '.jshintrc')]
+  paths: [path.join(__dirname, 'node_modules', 'standard', 'rc', '.jshintrc')]
 });
 
 replace({
   regex: '"disallowSemicolons": true,',
   replacement: '',
-  paths: [path.join(__dirname, 'node_modules', 'standard', 'lib', '.jscsrc')]
+  paths: [path.join(__dirname, 'node_modules', 'standard', 'rc', '.jscsrc')]
+});
+
+replace({
+  regex: '"semi": \\[2, "never"\\],',
+  replacement: '"semi": \[2, "always"\],',
+  paths: [path.join(__dirname, 'node_modules', 'standard', 'rc', '.eslintrc')]
 });
