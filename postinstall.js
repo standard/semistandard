72aa04d4cc593521001725d2c446d9983f37c9da
Dan Flettre <fletd01@yahoo.com>
2015-01-30 21:47:09 -0600

    update to standard 2.3.0

--- postinstall.js
+++ postinstall.js
@@ -2,12 +2,6 @@ var path = require('path');
 var replace = require('replace');
 
 replace({
-  regex: '"asi": true',
-  replacement: '"asi": false',
-  paths: [path.join(__dirname, 'node_modules', 'standard', 'rc', '.jshintrc')]
-});
-
-replace({
   regex: '"disallowSemicolons": true,',
   replacement: '',
   paths: [path.join(__dirname, 'node_modules', 'standard', 'rc', '.jscsrc')]
