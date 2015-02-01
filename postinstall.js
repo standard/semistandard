85f87420f680f95e8756697ad845c222ed70cf18
Dan Flettre <fletd01@yahoo.com>
2015-02-01 14:49:57 -0600

    remove jscs replace; rule is gone

--- postinstall.js
+++ postinstall.js
@@ -2,12 +2,6 @@ var path = require('path');
 var replace = require('replace');
 
 replace({
-  regex: '"disallowSemicolons": true,',
-  replacement: '',
-  paths: [path.join(__dirname, 'node_modules', 'standard', 'rc', '.jscsrc')]
-});
-
-replace({
   regex: '"semi": \\[2, "never"\\],',
   replacement: '"semi": \[2, "always"\],',
   paths: [path.join(__dirname, 'node_modules', 'standard', 'rc', '.eslintrc')]
