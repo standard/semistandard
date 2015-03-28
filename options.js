1e167c87d895296ab7f40db3b2dfcb257769cc83
Dan Flettre <fletd01@yahoo.com>
2015-03-28 02:54:21 -0500

    add explicit options

--- options.js
+++ options.js
@@ -1,6 +1,11 @@
 var path = require('path')
+var pkg = require('./package.json')
+
 module.exports = {
   // cmd, homepage, bugs all pulled from package.json
+  cmd: 'semistandard',
+  homepage: pkg.homepage,
+  bugs: pkg.bugs.url,
   tagline: 'Semicolons For All!',
   eslintConfig: {
       configFile: path.join(__dirname, 'eslintrc.json')
