1b691326c32cc3e4ae418b0adae9bd9b91ec39e4
Gustav Nikolaj Olsen <gno@one.com>
2015-11-06 20:26:56 +0100

    add "Use" to the tagline

--- options.js
+++ options.js
@@ -7,7 +7,7 @@ module.exports = {
   version: pkg.version,
   homepage: pkg.homepage,
   bugs: pkg.bugs.url,
-  tagline: 'Semicolons For All!',
+  tagline: 'Use Semicolons For All!',
   eslintConfig: {
     configFile: path.join(__dirname, 'eslintrc.json')
   },
