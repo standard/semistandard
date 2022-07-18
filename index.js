a505b9bbdd1a36db3539b5963730028c3e2aaacb
Pelle Wessman <pelle@kodfabrik.se>
2022-07-18 12:52:26 +0200

    Update `standard-engine`

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-/*! semistandard. MIT License. Feross Aboukhadijeh <https://feross.org/opensource> */
-// programmatic usage
-import engine from 'standard-engine'
-import opts from './options.js'
+/*! standard. MIT License. Feross Aboukhadijeh <https://feross.org/opensource> */
+import { StandardEngine } from 'standard-engine'
+import options from './options.js'
 
-const Linter = engine.linter
-
-export default new Linter(opts)
+export default new StandardEngine(options)
